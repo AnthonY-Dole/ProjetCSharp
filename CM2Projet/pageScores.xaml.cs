e383d57749326480d4c3524a7dcb4f1be2aa81c5
Anthony Dole <anthony.dole.pro@gmail.com>
2019-12-23 16:24:36 +0100

    Insertion du scoreboard avec valeur en dur

--- CM2Projet/pageScores.xaml.cs
+++ CM2Projet/pageScores.xaml.cs
@@ -12,6 +12,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Microsoft.Toolkit.Uwp.UI.Controls;
 
 // Pour plus d'informations sur le modèle d'élément Page vierge, consultez la page https://go.microsoft.com/fwlink/?LinkId=234238
 
@@ -20,13 +21,47 @@ namespace CM2Projet
     /// <summary>
     /// Une page vide peut être utilisée seule ou constituer une page de destination au sein d'un frame.
     /// </summary>
+
+  
+
+    public class Person
+    {
+      
+        public string Nom { get; set; }
+        public string TotalScore { get; set; }
+        public string ScoreFrancais { get; set; }
+        public string ScoreMath { get; set; }
+        public string ScoreGeographie { get; set; }
+        public string ScoreScience { get; set; }
+    }
     public sealed partial class pageScores : Page
     {
+        public List<Person> Persons { get; set; }
+
         public pageScores()
         {
             this.InitializeComponent();
-         
+
             AfficherDialogErreur();
+
+            Persons = new List<Person>
+        {
+            new Person
+            {
+               Nom = "Jean Eud", TotalScore = "12457",
+                ScoreFrancais = "54" ,ScoreMath ="125",ScoreGeographie="314",ScoreScience="456"
+            },
+            new Person
+            {
+                 Nom = "Rene isssou", TotalScore = "2457",
+                ScoreFrancais = "5544" ,ScoreMath ="125425",ScoreGeographie="31214",ScoreScience="497856"
+            },
+            new Person
+            {
+                 Nom = "Faze Arouf", TotalScore = "54457",
+                ScoreFrancais = "4561" ,ScoreMath ="781",ScoreGeographie="0546",ScoreScience="678"
+            }
+        };
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
@@ -66,4 +101,5 @@ namespace CM2Projet
             }
         }
     }
+   
 }
